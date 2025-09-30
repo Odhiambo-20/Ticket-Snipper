e4ee73c339eaae041ea8732a98bae9668ca6e045
Victor Odhiambo <odhiambov110@gmail.com>
2025-10-01 01:20:57 +0300

    Use post-install hook for google-services.json

--- eas-build-pre-prebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -n "$GOOGLE_SERVICES_JSON" ]; then
-  echo "$GOOGLE_SERVICES_JSON" | base64 -d > google-services.json
-  echo "✓ google-services.json created from environment variable"
-else
-  echo "⚠ GOOGLE_SERVICES_JSON environment variable not found"
-fi
